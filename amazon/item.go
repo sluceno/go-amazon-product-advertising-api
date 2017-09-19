14cc32db68d95c4b163622e3b1c39b000a017194
sluceno <sluceno.a@gmail.com>
2017-09-19 23:48:23 +0200

    Item description.

--- amazon/item.go
+++ amazon/item.go
@@ -17,21 +17,31 @@ type Items struct {
 
 // Item represents item
 type Item struct {
-	XMLName         xml.Name `xml:"Item"`
-	ASIN            string
-	DetailPageURL   string
-	SalesRank       int
-	ItemLinks       ItemLinks
-	SmallImage      Image
-	MediumImage     Image
-	LargeImage      Image
-	ImageSets       ImageSets
-	ItemAttributes  ItemAttributes
-	OfferSummary    OfferSummary
-	Offers          Offers
-	CustomerReviews CustomerReviews
-	SimilarProducts SimilarProducts
-	BrowseNodes     BrowseNodes
+	XMLName          xml.Name `xml:"Item"`
+	ASIN             string
+	DetailPageURL    string
+	SalesRank        int
+	ItemLinks        ItemLinks
+	SmallImage       Image
+	MediumImage      Image
+	LargeImage       Image
+	ImageSets        ImageSets
+	ItemAttributes   ItemAttributes
+	OfferSummary     OfferSummary
+	Offers           Offers
+	CustomerReviews  CustomerReviews
+	SimilarProducts  SimilarProducts
+	BrowseNodes      BrowseNodes
+	EditorialReviews EditorialReviews
+}
+
+type EditorialReviews struct {
+	EditorialReview []EditorialReview
+}
+
+type EditorialReview struct {
+	Source  string
+	Content string
 }
 
 // ItemLinks represents ItemLinks
@@ -131,6 +141,7 @@ type ItemAttributes struct {
 	Studio            string
 	Title             string
 	NumberOfDiscs     []int
+	Feature           []string
 }
 
 // Creator represents Creator
